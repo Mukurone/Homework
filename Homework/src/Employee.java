a144228bf217fe5336dcf48e332b8fbcd98c7fad
Mukuro <g3nri.ford@yandex.ru>
2024-05-27 22:16:22 +0300

    commit 4

---
+++ Homework/src/Employee.java
@@ -0,0 +1,38 @@
+public class Employee {
+    private String fullName;
+    private String position;
+    private String email;
+    private String phone;
+    private double salary;
+    private int age;
+
+    public Employee(String fullName, String position, String email, String phone, double salary, int age){
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+
+    }
+    public void informationDisplay(){
+        System.out.println("ФИО: " + fullName);
+        System.out.println("Должность: " + position);
+        System.out.println("Email: " + email);
+        System.out.println("Телефон: " + phone);
+        System.out.println("Зарплата: " + salary);
+        System.out.println("Возраст: " + age);
+    }
+
+    public static void main(String[] args){
+        Employee employee = new Employee(
+                "Шакаров Тимур Маратович",
+                "Тестировщик",
+                "srmv4334@gmail.com",
+                "89302778782",
+                123.21,
+                26
+        );
+        employee.informationDisplay();
+    }
+}
--- Homework/src/Main.java
+++ Homework/src/Main.java
@@ -1,15 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        //task10 method = new task10();
-        //task10.arr();
-        //System.out.println(task9.year(2019));
 
-        int len = 3;
-        int initialValue = 7;
-        int[] array = task14.arrCreate(len, initialValue);
-        for (int i = 0; i < len; i++){
-            System.out.println(array[i] + " ");
-        }
     }
-
 }
