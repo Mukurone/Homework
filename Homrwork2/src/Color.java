2b076c4e97abf9e2d4da6e594d8d8b81f74ebdc2
Mukuro <g3nri.ford@yandex.ru>
2024-06-13 23:27:52 +0300

    Commit дз 1

---
+++ Homrwork2/src/Color.java
@@ -0,0 +1,4 @@
+public interface Color {
+    String getFillColor();
+    String getBorderColor();
+}
--- Homrwork2/src/Main.java
+++ Homrwork2/src/Main.java
@@ -37,5 +37,27 @@ public class Main {
         System.out.println("Всего животных: " + Animal.getAnimalCount());
         System.out.println("Всего собак: " + Dog.getDogCount());
         System.out.println("Всего котов: " + Cat.getCatCount());
+
+
+
+        Shape circle = new Circle(5, "red", "black");
+        Shape rectangle = new Rectangle(2, 3, "red", "black;");
+        Shape triangle = new Triangle(1, 2, 3, "red", "black");
+
+        printShapeInfo(circle);
+        printShapeInfo(rectangle);
+        printShapeInfo(triangle);
+
+    }
+
+    public static void printShapeInfo(Shape shape){
+        if (shape instanceof Color){
+            Color color = (Color) shape;
+            System.out.println("Периметр: " + shape.getPerimeter());
+            System.out.println("Площадь: " + shape.getArea());
+            System.out.println("Цвет заливки: " + color.getFillColor());
+            System.out.println("Цвет границы: " + color.getBorderColor());
+            System.out.println();
+        }
     }
 }
