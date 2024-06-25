f449adc7f63ea3fa0afe4122453d6f0baedd3052
Mukuro <g3nri.ford@yandex.ru>
2024-06-25 22:46:58 +0300

    commit testNG 1

---
+++ src/test/java/example/FactorialCalculatorTest.java
@@ -0,0 +1,23 @@
+package example;
+
+import org.example.FactorialCalculator;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+public class FactorialCalculatorTest {
+
+    @Test
+    public void testCalculateFactorial() {
+        assertEquals(FactorialCalculator.calculateFactorial(0), 1);
+        assertEquals(FactorialCalculator.calculateFactorial(1), 1);
+        assertEquals(FactorialCalculator.calculateFactorial(2), 2);
+        assertEquals(FactorialCalculator.calculateFactorial(3), 6);
+        assertEquals(FactorialCalculator.calculateFactorial(4), 24);
+        assertEquals(FactorialCalculator.calculateFactorial(5), 120);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "Число должно быть положительным")
+    public void testCalculateFactorialNegativeNumber() {
+        FactorialCalculator.calculateFactorial(-1);
+    }
+}
